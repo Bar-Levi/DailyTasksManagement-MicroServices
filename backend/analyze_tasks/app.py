9fefad18a942b24c4f2d515da6a5f874c3d6c5a2
Romihia <118122586+Romihia@users.noreply.github.com>
2025-01-15 14:05:36 +0200

    Added task mock

---
+++ backend/analyze_tasks/app.py
@@ -0,0 +1,10 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def mock_service():
+    return jsonify({'message': 'This is a future service: analyze_tasks'}), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)  
