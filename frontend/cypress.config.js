ea1a8e8275cfcddcce9be0c9da2920052c7a7d49
Nir Hazan <nirhazan7@gmail.com>
2025-01-27 12:36:12 +0200

    .

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -6,5 +6,7 @@ module.exports = defineConfig({
     viewportWidth: 1280,
     viewportHeight: 720,
     supportFile: 'cypress/support/e2e.js',
+    screenshotOnRunFailure: false, // Disable screenshots on test failure
   },
 });
+
